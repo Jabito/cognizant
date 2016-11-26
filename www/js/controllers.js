caac90151e7754aba6c7a4530e33031f52369feb
Jabito <jabito.javier@gmail.com>
2016-11-27 07:02:44 +0800

    push jabs

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,6 +1,6 @@
 angular.module('starter.controllers', [])
 
-    .controller('AppCtrl', function ($scope, $ionicModal, $timeout, $ionicSlideBoxDelegate) {
+    .controller('AppCtrl', function($scope, $ionicModal, $timeout, $ionicSlideBoxDelegate) {
 
         /*//SQL Initialization
         var db = $cordovaSQLite.openDB({ name: "my.db" });
@@ -16,6 +16,12 @@ angular.module('starter.controllers', [])
                 console.error(err);
             });
         };*/
+        //Setting up some static data
+        $scope.employeenumber = "4253627";
+        $scope.username = "Jon Snow";
+        $scope.useremail = "jonsnow@cognizant.com";
+        $scope.userimage = "img/leopic.jpg";
+        $scope.tokens = 5;
 
         // Form data for the login modal
         $scope.loginData = {};
@@ -23,107 +29,84 @@ angular.module('starter.controllers', [])
         // Create the login modal that we will use later
         $ionicModal.fromTemplateUrl('templates/login.html', {
             scope: $scope
-        }).then(function (modal) {
+        }).then(function(modal) {
             $scope.modal = modal;
         });
 
         // Triggered in the login modal to close it
-        $scope.closeLogin = function () {
+        $scope.closeLogin = function() {
             $scope.modal.hide();
         };
 
         // Open the login modal
-        $scope.login = function () {
+        $scope.login = function() {
             $scope.modal.show();
         };
 
-
-        $scope.employeenumber = "4253627";
-        $scope.username = "Jon Snow";
-        $scope.useremail = "jonsnow@cognizant.com";
-        $scope.userimage = "img/leopic.jpg";
-        $scope.tokens = 5;
         // Perform the login action when the user submits the login form
-        $scope.doLogin = function () {
+        $scope.doLogin = function() {
             console.log('Doing login', $scope.loginData);
 
             // Simulate a login delay. Remove this and replace with your login
             // code if using a login system
-            $timeout(function () {
+            $timeout(function() {
                 $scope.closeLogin();
             }, 1000);
         };
     })
 
-    .controller('ProfileCtrl', function ($scope, $stateParams, $state, $ionicSlideBoxDelegate) {
+    .controller('ProfileCtrl', function($scope, $stateParams, $state, $ionicSlideBoxDelegate, Medals) {
         // Called to navigate to the main app
-        $scope.startApp = function () {
+        $scope.startApp = function() {
             $state.go('main');
         };
-        $scope.next = function () {
+        $scope.next = function() {
             $ionicSlideBoxDelegate.next();
         };
-        $scope.previous = function () {
+        $scope.previous = function() {
             $ionicSlideBoxDelegate.previous();
         };
 
         // Called each time the slide changes
-        $scope.slideChanged = function (index) {
+        $scope.slideChanged = function(index) {
             $scope.slideIndex = index;
         };
 
-        //This holds the achievements the user currently has.
-        $scope.medaldata = [{
-            "title": "Attendance",
-            "rank": "gold",
-            "max": "50",
-            "current": "40"
-        }, {
-            "title": "Referrals",
-            "rank": "bronze",
-            "max": "30",
-            "current": "3"
-        }, {
-            "title": "Volunteering",
-            "rank": "silver",
-            "max": "20",
-            "current": "8"
-        }, {
-            "title": "Attendance",
-            "rank": "bronze",
-            "max": "12",
-            "current": "5"
-        }, {
-            "title": "Perfect Attendance Achievement",
-            "rank": "gold",
-            "max": "50",
-            "current": "40"
-        }, {
-            "title": "Referrals",
-            "rank": "bronze",
-            "max": "30",
-            "current": "3"
-        }, {
+        console.log("hit");
+        //Medals data from JSON
 
-            "title": "Volounteering",
-            "rank": "silver",
-            "max": "20",
-            "current": "8"
-        }, {
-            "title": "Attendance Achievement",
-            "rank": "bronze",
-            "max": "12",
-            "current": "5"
-        }];
-    })
-
-    .controller('AchievementCtrl', function ($scope, $stateParams) {
+        Medals.all().success(function(response) {
+            $scope.medals = response;
+        });
 
     })
 
-    .controller('ReferralCtrl', function ($scope, $stateParams) {
+    .controller('VolunteerCtrl', function($scope, $stateParams, FVols, PVols) {
+
+        FVols.all().success(function(response) {
+            $scope.fvols = response;
+        });
+        PVols.all().success(function(response) {
+            $scope.pvols = response;
+        });
+
+    })
+
+    .controller('AchievementCtrl', function($scope, $stateParams) {
+
+
+    })
+
+    .controller('ReferralCtrl', function($scope, $stateParams, Referrals) {
+
+        //Referrals Data from JSON
+        Referrals.all().success(function(response) {
+            $scope.referrals = response;
+        });
+
+
         //This holds all the data for referrals the user has.
-        $scope.referrals = [{
+        /*$scope.referrals = [{
             "name": "Paolo Calaguian",
             "imageurl": "img/paopic.jpg",
             "StartDate": "November 21,2014",
@@ -166,5 +149,5 @@ angular.module('starter.controllers', [])
             "StartDate": "March 18, 2005",
             "TokensReceived": "20"
 
-        }];
+        }];*/
     });
--- www/js/services.js
+++ www/js/services.js
@@ -1,124 +1,57 @@
-var myModule = angular.module('myModule', []);
+angular.module('starter.services', [])
 
-myModule.factory('UserApi', function($http, $q, ApiEndpoint){
-  var loginPost = function(useremail, password) {
-    return $http({
-      method: 'post',
-      url: ApiEndpoint.url + '/api-x/users/login', 
-      data: {
-        useremail: useremail,
-        password: password
-      }
-    });
-  }
-
-  var registerPost = function(/* TODO Missing parameters */) {
-    var q = $q.defer();
-
-    $http.post(ApiEndpoint.url + '/api-x/users/register', {
-      /* TODO Missing data */
+    .factory('Referrals', function ($http, $rootScope, $stateParams) {
+        return {
+            all: function () {
+                return $http.get('data/referrals.json', { params: { user_id: $rootScope.session } })
+            },
+            get: function () {
+                return $http.get('data/referrals.json', { params: { user_id: $rootScope.session, chat_id: $stateParams.idchat } })
+            },
+            add: function (id) {
+                return $http.get('data/referrals.json', { params: { idfriends: id } })
+            }
+        };
     })
-    .success(function (data) { q.resolve(data); })
-    .error(function (error) { q.reject(error); });
 
-    return q.promise;
-  }
-
-  var uidGet = function(email){
-    return $http({
-      method: 'get',
-      url: ApiEndpoint.url + '/api-x/users/getuid',
-      data: {
-        email: email
-      }
+    .factory('Medals', function ($http, $rootScope, $stateParams) {
+        return {
+            all: function () {
+                return $http.get('data/medals.json', { params: { user_id: $rootScope.session } })
+            },
+            get: function () {
+                return $http.get('data/medals.json', { params: { user_id: $rootScope.session, chat_id: $stateParams.idchat } })
+            },
+            add: function (id) {
+                return $http.get('data/medals.json', { params: { idfriends: id } })
+            }
+        };
+    })
+    
+    .factory('FVols', function ($http, $rootScope, $stateParams) {
+        return {
+            all: function () {
+                return $http.get('data/volunteerfuture.json', { params: { user_id: $rootScope.session } })
+            },
+            get: function () {
+                return $http.get('data/volunteerfuture.json', { params: { user_id: $rootScope.session, chat_id: $stateParams.idchat } })
+            },
+            add: function (id) {
+                return $http.get('data/volunteerfuture.json', { params: { idfriends: id } })
+            }
+        };
+    })
+    
+    .factory('PVols', function ($http, $rootScope, $stateParams) {
+        return {
+            all: function () {
+                return $http.get('data/volunteerpast.json', { params: { user_id: $rootScope.session } })
+            },
+            get: function () {
+                return $http.get('data/volunteerpast.json', { params: { user_id: $rootScope.session, chat_id: $stateParams.idchat } })
+            },
+            add: function (id) {
+                return $http.get('data/volunteerpast.json', { params: { idfriends: id } })
+            }
+        };
     });
-  }
-
-  return {
-    loginPost: loginPost,
-    registerPost: registerPost
-  }
-});
-
-myModule.factory('Session', function($resource){
-  return $resource('http://localhost:5000/sessions/:sessionId');
-});
-
-myModule.factory('Chats', function() {
-  // Might use a resource here that returns a JSON array
-
-  // Some fake testing data
-  var chats = [{
-    id: 0,
-    name: 'Ben Sparrow',
-    lastText: 'You on your way?',
-    face: 'img/ben.png'
-  }, {
-    id: 1,
-    name: 'Max Lynx',
-    lastText: 'Hey, it\'s me',
-    face: 'img/max.png'
-  }, {
-    id: 2,
-    name: 'Adam Bradleyson',
-    lastText: 'I should buy a boat',
-    face: 'img/adam.jpg'
-  }, {
-    id: 3,
-    name: 'Perry Governor',
-    lastText: 'Look at my mukluks!',
-    face: 'img/perry.png'
-  }, {
-    id: 4,
-    name: 'Mike Harrington',
-    lastText: 'This is wicked good ice cream.',
-    face: 'img/mike.png'
-  }];
-
-  return {
-    all: function() {
-      return chats;
-    },
-    remove: function(chat) {
-      chats.splice(chats.indexOf(chat), 1);
-    },
-    get: function(chatId) {
-      for (var i = 0; i < chats.length; i++) {
-        if (chats[i].id === parseInt(chatId)) {
-          return chats[i];
-        }
-      }
-      return null;
-    }
-  };
-});
-
-myModule.factory('JSONData', function ($http, $rootScope, $stateParams) {
-
-  return {
-    all: function () {
-      return $http.get('lib/somedata.json', { params: { user_id: $rootScope.session } })
-    },
-    get: function () {
-      return $http.get('lib/somedata.json', { params: { user_id: $rootScope.session, chat_id: $stateParams.idchat } })
-    },
-    add: function (id) {
-      return $http.get('lib/somedata.json', { params: {idfriends:id}})
-    }
-  };
-});
-
-myModule.factory('EVENTS', function ($http, $rootScope, $stateParams) {
-
-  return {
-    all: function () {
-      return $http.get('lib/events.json', { params: { user_id: $rootScope.session } })
-    },
-    get: function () {
-      return $http.get('lib/events.json', { params: { user_id: $rootScope.session, chat_id: $stateParams.idchat } })
-    },
-    add: function (id) {
-      return $http.get('lib/events.json', { params: {idfriends:id}})
-    }
-  };
-});
